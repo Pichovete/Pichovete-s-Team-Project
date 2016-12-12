30b5d77327ea94c46a4a889614c7bc2e43c32ae3
dstoykov <maverick1997@abv.bg>
2016-12-12 15:36:54 +0200

    Startup project

---
+++ src/main/java/softuniBlog/controller/UserController.java
@@ -0,0 +1,137 @@
+package softuniBlog.controller;
+
+import org.apache.catalina.servlet4preview.http.HttpServletRequest;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.web.authentication.logout.SecurityContextLogoutHandler;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.util.StringUtils;
+import org.springframework.web.bind.annotation.*;
+import softuniBlog.bindingModel.UserBindingModel;
+import softuniBlog.bindingModel.UserEditBindingModel;
+import softuniBlog.entity.Role;
+import softuniBlog.entity.User;
+import softuniBlog.repository.RoleRepository;
+import softuniBlog.repository.UserRepository;
+
+import javax.servlet.http.HttpServletResponse;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
+@Controller
+public class UserController {
+
+    @Autowired
+    RoleRepository roleRepository;
+    @Autowired
+    UserRepository userRepository;
+
+    @GetMapping("/register")
+    public String register(Model model) {
+        model.addAttribute("view", "user/register");
+
+        return "base-layout";
+    }
+
+    @PostMapping("/register")
+    public String registerProcess(UserBindingModel userBindingModel){
+
+        if(!userBindingModel.getPassword().equals(userBindingModel.getConfirmPassword())){
+            return "redirect:/register";
+        }
+
+        BCryptPasswordEncoder bCryptPasswordEncoder = new BCryptPasswordEncoder();
+
+        User user = new User(
+                userBindingModel.getEmail(),
+                userBindingModel.getFullName(),
+                bCryptPasswordEncoder.encode(userBindingModel.getPassword()),
+                userBindingModel.getAddress()
+        );
+
+        Role userRole = this.roleRepository.findByName("ROLE_USER");
+
+        user.addRole(userRole);
+
+        this.userRepository.saveAndFlush(user);
+
+        return "redirect:/login";
+    }
+
+    @GetMapping("/login")
+    public String login(Model model){
+        model.addAttribute("view", "user/login");
+
+        return "base-layout";
+    }
+
+    @RequestMapping(value="/logout", method = RequestMethod.GET)
+    public String logoutPage (HttpServletRequest request, HttpServletResponse response) {
+        Authentication auth = SecurityContextHolder.getContext().getAuthentication();
+
+        if (auth != null) {
+            new SecurityContextLogoutHandler().logout(request, response, auth);
+        }
+
+        return "redirect:/login?logout";
+    }
+
+    @GetMapping("/profile")
+    @PreAuthorize("isAuthenticated()")
+    public String profilePage(Model model){
+        UserDetails principal = (UserDetails) SecurityContextHolder.getContext()
+                .getAuthentication()
+                .getPrincipal();
+
+        User user = this.userRepository.findByEmail(principal.getUsername());
+
+        model.addAttribute("user", user);
+        model.addAttribute("view", "user/profile");
+
+        return "base-layout";
+    }
+
+    @GetMapping("/user/edit/{id}")
+    @PreAuthorize("isAuthenticated()")
+    public String edit(@PathVariable Integer id, Model model){
+
+
+        User user = this.userRepository.findOne(id);
+
+        model.addAttribute("user", user);
+        model.addAttribute("view", "user/edit");
+
+        return "base-layout";
+    }
+    @PostMapping("/user/edit/{id}")
+    public String editProcess(@PathVariable Integer id,
+                              UserEditBindingModel userBindingModel){
+
+        User user = this.userRepository.findOne(id);
+
+        if(!StringUtils.isEmpty(userBindingModel.getPassword())
+                && !StringUtils.isEmpty(userBindingModel.getConfirmPassword())){
+
+            if(userBindingModel.getPassword().equals(userBindingModel.getConfirmPassword())){
+                BCryptPasswordEncoder bCryptPasswordEncoder = new BCryptPasswordEncoder();
+
+                user.setPassword(bCryptPasswordEncoder.encode(userBindingModel.getPassword()));
+            }
+        }
+
+        user.setFullName(userBindingModel.getFullName());
+        user.setEmail(userBindingModel.getEmail());
+        user.setAddress(userBindingModel.getAddress());
+
+        this.userRepository.saveAndFlush(user);
+
+        return "redirect:/profile";
+    }
+}
+
---
+++ src/main/java/softuniBlog/entity/User.java
@@ -0,0 +1,121 @@
+package softuniBlog.entity;
+
+import javax.persistence.*;
+import java.util.HashSet;
+import java.util.Objects;
+import java.util.Set;
+
+@Entity
+@Table(name = "users")
+public class User {
+
+    private Integer id;
+
+    private String email;
+
+    private String fullName;
+
+    private String password;
+
+    private String address;
+
+    private Set<Role> roles;
+
+    private Set<Article> articles;
+
+
+    public User(String email, String fullName, String password, String address) {
+        this.email = email;
+        this.password = password;
+        this.fullName = fullName;
+        this.address = address;
+
+        this.roles = new HashSet<>();
+
+        this.articles = new HashSet<>();
+    }
+
+    public User() {    }
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    @Column(name = "email", unique = true, nullable = false)
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    @Column(name = "fullName", nullable = false)
+    public String getFullName() {
+        return fullName;
+    }
+
+    public void setFullName(String fullName) {
+        this.fullName = fullName;
+    }
+
+    @Column(name = "password", length = 60, nullable = false)
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    @ManyToMany(fetch = FetchType.EAGER)
+    @JoinTable(name = "users_roles")
+    public Set<Role> getRoles() {
+        return roles;
+    }
+
+    public void setRoles(Set<Role> roles) {
+        this.roles = roles;
+    }
+
+    public void addRole(Role role) {
+        this.roles.add(role);
+    }
+
+    @Column(name = "address")
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    @OneToMany(mappedBy = "author")
+    public Set<Article> getArticles(){
+        return articles;
+    }
+
+    public void setArticles(Set<Article> articles){
+        this.articles = articles;
+    }
+
+    @Transient
+    public boolean isAdmin(){
+        return this.getRoles()
+                .stream()
+                .anyMatch(role -> role.getName().equals("ROLE_ADMIN"));
+    }
+
+    @Transient
+    public boolean isAuthor(Article article){
+        return Objects.equals(this.getId(),
+                article.getAuthor().getId());
+    }
+}
